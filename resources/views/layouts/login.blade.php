05834b654236d0ada734362988fc35d63be78022
望月莉緒 <>
2022-11-28 15:46:48 +0900

    commit

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -22,18 +22,20 @@
 <body>
     <header>
         <div id = "head">
-        <h1><a><img src="images/logo.png"></a></h1>
+        <h1><a href="/top"><img src="images/atlas.png"></a></h1>
             <div id="">
-                <div id="">
-                    <p>〇〇さん<img src="images/arrow.png"></p>
-                <div>
+                <div id="accordion" class="accordion-container">
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+                    <script src="js/script.js">
+                    <p>〇〇さん<img src="images/icon4.png"></p>
+                    </div>
                 <ul>
                     <li><a href="/top">ホーム</a></li>
                     <li><a href="/profile">プロフィール</a></li>
                     <li><a href="/logout">ログアウト</a></li>
                 </ul>
-            </div>
-        </div>
+                </script>
+                </div>
     </header>
     <div id="row">
         <div id="container">
@@ -46,14 +48,14 @@
                 <p>フォロー数</p>
                 <p>〇〇名</p>
                 </div>
-                <p class="btn"><a href="">フォローリスト</a></p>
+                <p class="btn"><a href="/follow-list">フォローリスト</a></p>
                 <div>
                 <p>フォロワー数</p>
                 <p>〇〇名</p>
                 </div>
-                <p class="btn"><a href="">フォロワーリスト</a></p>
+                <p class="btn"><a href="/follower-list">フォロワーリスト</a></p>
             </div>
-            <p class="btn"><a href="">ユーザー検索</a></p>
+            <p class="btn"><a href="/search">ユーザー検索</a></p>
         </div>
     </div>
     <footer>
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.login')
 
 @section('content')
-
+<form action="" method="get">
+<input type="search" name="search">
+<input type="submit" name="submit" value="検索">
+</form>
 
 
 @endsection
