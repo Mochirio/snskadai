6b1fd9a98d52fbee7e7e0695853a83b8c101eee3
望月莉緒 <>
2022-12-06 11:44:30 +0900

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,9 @@ jQuery(function ($) {
 
   $('.js-accordion-title').on('click', function () {
     /*クリックでコンテンツを開閉*/
-    $('accordion-content').slideToggle();
+    $('accordion-content').slideToggle(300);
     /*矢印の向きを変更*/
-    $(this).toggleClass('active');
+    $(this).toggleClass('open', 300);
   })
 
 });
